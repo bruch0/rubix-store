dcbb3d2def3645a948a03e72c3740d319262ba1f
bruch0 <lucas.bruch0@gmail.com>
2021-11-09 11:47:56 -0300

    fix: removed console.log

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 const api = axios.create({ baseURL: process.env.NODE_ENV === 'production' ? 'https://rubix-store.herokuapp.com/' : 'http://localhost:4000/' });
-console.log(process.env);
 
 export default api;
