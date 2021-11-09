fdfebd3fd6199be5a88f86376ae11d165c188086
bruch0 <lucas.bruch0@gmail.com>
2021-11-09 11:34:10 -0300

    fix: env not working

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({ baseURL: process.env.NODE_ENV === 'prod' ? 'https://rubix-store.herokuapp.com/' : 'http://localhost:4000/' });
+console.log(process.env);
 
 export default api;
