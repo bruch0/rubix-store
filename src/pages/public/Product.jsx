c937d13a21c8a5eb9307cd4d6e18a51c3e3a305b
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-12 18:18:20 -0300

    feat: carrosel products images

--- src/pages/public/Product.jsx
+++ src/pages/public/Product.jsx
@@ -1,29 +1,115 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export default function Product() {
   useEffect(() => {}, []);
+  const [indexImage, setIndexImage] = useState(0);
+
+  const images = [
+    {
+      url: 'https://www.ziicube.com/image/cube/Gan/12-Maglev/GAN-12-01.jpg',
+    },
+    {
+      url: 'https://www.ziicube.com/image/cube/Gan/12-Maglev/GAN-12-02.jpg',
+    },
+    {
+      url: 'https://www.ziicube.com/image/cube/Gan/12-Maglev/GAN-12-03.jpg',
+    },
+    {
+      url: 'https://www.ziicube.com/image/cube/Gan/12-Maglev/GAN-12-04.jpg',
+    },
+  ];
+
+  // eslint-disable-next-line no-unused-vars
+  function controlPicture(n) {
+    if (indexImage === 0 && n === -1) {
+      setIndexImage(images.length - 1);
+    } else if (indexImage === images.length - 1 && n === 1) {
+      setIndexImage(0);
+    } else {
+      setIndexImage(indexImage + n);
+    }
+  }
+
   return (
-    <ContainerProduct>
-      <ContainerTopLinks>
-        <TopLink to="/">Home</TopLink>
-        <ArrowLinks />
-        <TopLink to="/">3x3x3</TopLink>
-      </ContainerTopLinks>
-      <TitleProduct>
-        Cubo Mágico Profissional GAN 356 RS stickerless 3x3x3
-      </TitleProduct>
-      <ContainerPictureShow>A</ContainerPictureShow>
-    </ContainerProduct>
+    <Main>
+      <ContainerProduct>
+        <ContainerTopLinks>
+          <TopLink to="/">Home</TopLink>
+          <ArrowLinks />
+          <TopLink to="/">3x3x3</TopLink>
+        </ContainerTopLinks>
+        <TitleProduct>
+          Cubo Mágico Profissional GAN 356 RS stickerless 3x3x3
+        </TitleProduct>
+        <ContainerPictureShow>
+          <PictureNumberText>
+            {indexImage + 1}
+            /
+            {images.length}
+          </PictureNumberText>
+          <Picture src={images[indexImage]?.url} alt="Imagem" />
+          <ArrowPassPrev onClick={() => controlPicture(-1)}>
+            &#10094;
+          </ArrowPassPrev>
+          <ArrowPassNext onClick={() => controlPicture(1)}>
+            &#10095;
+          </ArrowPassNext>
+        </ContainerPictureShow>
+      </ContainerProduct>
+    </Main>
   );
 }
 
+const Main = styled.main`
+  display: flex;
+
+`;
+
+const ArrowPassPrev = styled.div`
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: black;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const ArrowPassNext = styled(ArrowPassPrev)`
+  right: 0;
+  border-radius: 3px 0 0 3px;
+`;
+
+const Picture = styled.img`
+  width: 100%;
+  height: 100%;
+  margin: auto 0;
+  object-fit: cover;
+`;
+
+const PictureNumberText = styled.div`
+  color: #000000;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+`;
+
 const ContainerPictureShow = styled.div`
-  max-width: 500px;
-  background-color: red;
+  width: 500px;
+  height: 500px;
   position: relative;
-  margin: auto;
+  
 `;
 
 const TitleProduct = styled.h1`
@@ -33,7 +119,7 @@ const TitleProduct = styled.h1`
 `;
 
 const ContainerProduct = styled.div`
-  margin: 130px 5%;
+  margin: 130px auto;
 `;
 
 const ContainerTopLinks = styled.div`
