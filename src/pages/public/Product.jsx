aad8153a4a81b411d06605f642395e836a7485eb
bruch0 <lucas.bruch0@gmail.com>
2021-11-13 19:27:49 -0300

    fix: removed logs

--- src/pages/public/Product.jsx
+++ src/pages/public/Product.jsx
@@ -49,14 +49,11 @@ export default function Product() {
   function handleCalculateShipping() {
     setIsLoading(true);
     senderInfo.sCepDestino = cep;
-    console.log(senderInfo);
     calcularPrecoPrazo(senderInfo)
-      .then((res) => {
-        console.log(res);
+      .then(() => {
         setIsLoading(false);
       })
       .catch((err) => {
-        console.log(err);
         throwError(err);
       });
   }
@@ -71,8 +68,7 @@ export default function Product() {
 
   useEffect(() => {
     getProduct(productId)
-      .then((res) => setProduct(res.data))
-      .catch((err) => console.log(err));
+      .then((res) => setProduct(res.data));
   }, []);
 
   if (product.length === 0) {
