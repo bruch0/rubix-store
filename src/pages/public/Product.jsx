badbf92dbd9a4824bcc2cec5a04e85086c1d20d2
Carlos Gabriel <carlosgabrielpc@live.com>
2021-11-12 22:02:10 -0300

    feat: sidebar layout

--- src/pages/public/Product.jsx
+++ src/pages/public/Product.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import InputForm from '../../components/InputForm';
+import { ReactComponent as ShippingIcon } from '../../assets/icons/shipping-fast.svg';
 
 export default function Product() {
   useEffect(() => {}, []);
   const [indexImage, setIndexImage] = useState(0);
+  const [cep, setCep] = useState('');
 
   const images = [
     {
@@ -34,7 +39,7 @@ export default function Product() {
 
   return (
     <Main>
-      <ContainerProduct>
+      <ContainerCenter>
         <ContainerTopLinks>
           <TopLink to="/">Home</TopLink>
           <ArrowLinks />
@@ -43,28 +48,154 @@ export default function Product() {
         <TitleProduct>
           Cubo Mágico Profissional GAN 356 RS stickerless 3x3x3
         </TitleProduct>
-        <ContainerPictureShow>
-          <PictureNumberText>
-            {indexImage + 1}
-            /
-            {images.length}
-          </PictureNumberText>
-          <Picture src={images[indexImage]?.url} alt="Imagem" />
-          <ArrowPassPrev onClick={() => controlPicture(-1)}>
-            &#10094;
-          </ArrowPassPrev>
-          <ArrowPassNext onClick={() => controlPicture(1)}>
-            &#10095;
-          </ArrowPassNext>
-        </ContainerPictureShow>
-      </ContainerProduct>
+        <ContainerProduct>
+          <ContainerPictureShow>
+            <PictureNumberText>
+              {indexImage + 1}/{images.length}
+            </PictureNumberText>
+            <Picture src={images[indexImage]?.url} alt="Imagem" />
+            <ArrowPassPrev onClick={() => controlPicture(-1)}>
+              &#10094;
+            </ArrowPassPrev>
+            <ArrowPassNext onClick={() => controlPicture(1)}>
+              &#10095;
+            </ArrowPassNext>
+          </ContainerPictureShow>
+          <Sidebar>
+            <Price>R$ 129,99</Price>
+            <HorizontalLine />
+            <QuantityContainer>
+              <SelectQuantity name="product_quantity">
+                {[...Array(10).keys()].map((e) => (
+                  <option>{e + 1}</option>
+                ))}
+              </SelectQuantity>
+              <AvailableQuantity>27 disponíveis</AvailableQuantity>
+            </QuantityContainer>
+            <ShippingCostContainer>
+              <p>
+                Calcular <span>frete e prazo</span>
+              </p>
+              <FieldShippingContainer>
+                <InputShippingCost
+                  maxLength={9}
+                  value={cep}
+                  onChange={(e) => setCep(e.target.value.replace(/\D/g, '').replace(/^(\d{5})(\d{3})+?$/, '$1-$2'))}
+                />
+                <ButtonShippingCost>
+                  <ShippingIcon />
+                </ButtonShippingCost>
+              </FieldShippingContainer>
+            </ShippingCostContainer>
+          </Sidebar>
+        </ContainerProduct>
+      </ContainerCenter>
     </Main>
   );
 }
 
+const FieldShippingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 7px;
+`;
+
+const ButtonShippingCost = styled.button`
+  width: 74px;
+  height: 35px;
+  outline: none;
+  border: none;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 22px;
+  cursor: pointer;
+`;
+
+const InputShippingCost = styled(InputForm)`
+  background-color: #fff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25) !important;
+  width: 72%;
+  margin-bottom: 0;
+`;
+
+const ShippingCostContainer = styled.div`
+  margin: 30px 0px;
+  p {
+    font-weight: 500;
+    font-size: 18px;
+    margin-left: 5px;
+  }
+  span {
+    font-weight: bold;
+  }
+`;
+
+const AvailableQuantity = styled.p`
+  font-weight: 500;
+  font-size: 20px;
+  color: #737070;
+`;
+
+const SelectQuantity = styled.select`
+  font-family: 'Quicksand', sans-serif;
+  width: 150px;
+  height: 40px;
+  padding-left: 50px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 22px;
+  border: none;
+  font-weight: 500;
+  font-size: 24px;
+  outline: none;
+  appearance: none;
+  background: #fff
+    url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 330 330' style='enable-background:new 0 0 330 330' xml:space='preserve'%3E%3Cpath d='M325.607 79.393c-5.857-5.857-15.355-5.858-21.213.001l-139.39 139.393L25.607 79.393c-5.857-5.857-15.355-5.858-21.213.001-5.858 5.858-5.858 15.355 0 21.213l150.004 150a14.999 14.999 0 0 0 21.212-.001l149.996-150c5.859-5.857 5.859-15.355.001-21.213z'/%3E%3C/svg%3E")
+    no-repeat;
+  background-position: right 15px top 50%;
+  background-size: 30px;
+`;
+
+const QuantityContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+`;
+
+const HorizontalLine = styled.div`
+  width: 110%;
+  border-top: 2px #686565 solid;
+  margin: 15px -15px;
+`;
+
+const Price = styled.h2`
+  color: #1382e9;
+  font-weight: bold;
+  font-size: 40px;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+const ContainerProduct = styled.div`
+  width: 900px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Sidebar = styled.div`
+  width: 345px;
+  padding: 14px 20px;
+  background: #ebebeb;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 22px;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Main = styled.main`
   display: flex;
-
 `;
 
 const ArrowPassPrev = styled.div`
@@ -95,6 +226,7 @@ const Picture = styled.img`
   height: 100%;
   margin: auto 0;
   object-fit: cover;
+  border-radius: 7px;
 `;
 
 const PictureNumberText = styled.div`
@@ -109,7 +241,6 @@ const ContainerPictureShow = styled.div`
   width: 500px;
   height: 500px;
   position: relative;
-  
 `;
 
 const TitleProduct = styled.h1`
@@ -118,7 +249,7 @@ const TitleProduct = styled.h1`
   margin: 30px 0;
 `;
 
-const ContainerProduct = styled.div`
+const ContainerCenter = styled.div`
   margin: 130px auto;
 `;
 
